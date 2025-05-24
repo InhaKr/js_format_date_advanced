abdff97bea590449721a999063277562a588e1ac
InhaKr <inkr3891@gmail.com>
2025-05-24 21:25:17 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,55 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  let qwer;
+  let d;
+  const v = fromFormat[fromFormat.length - 1];
+
+  const r = date.split(v);
+
+  for (let i = 0; i < r.length; i++) {
+    for (let k = 0; k < toFormat.length - 1; k++) {
+      d = toFormat[toFormat.length - 1];
+
+      if (toFormat[k] === fromFormat[i]) {
+        toFormat[k] = r[i];
+      }
+
+      if (toFormat[k] < fromFormat[i]) {
+        for (const char of toFormat[k]) {
+          for (const xar of fromFormat[i]) {
+            if (xar === char) {
+              const raw = r[i].split('').splice(2, 2).join('');
+
+              toFormat[k] = raw;
+            }
+          }
+        }
+      }
+
+      if (toFormat[k].length > fromFormat[i].length) {
+        for (const rad of toFormat[k]) {
+          for (const dar of fromFormat[i]) {
+            if (dar === rad) {
+              qwer = r[i];
+            }
+
+            if (qwer < 30) {
+              toFormat[k] = '20' + qwer;
+            }
+
+            if (qwer >= 30) {
+              toFormat[k] = '19' + qwer;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  toFormat.length = 3;
+
+  return toFormat.join(d);
 }
 
 module.exports = formatDate;

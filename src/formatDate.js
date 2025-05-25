62de5ad7d4f73fe84fc88d0838a7cbb79b319f14
InhaKr <inkr3891@gmail.com>
2025-05-25 15:42:43 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,8 +8,11 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  const oldSeparate = fromFormat.splice(3, 1);
-  const newSeparate = toFormat.splice(3, 1);
+  const oldSeparate = fromFormat[fromFormat.length - 1];
+  // console.log(oldSeparate)
+  const newSeparate = toFormat[toFormat.length - 1];
+  // console.log(newSeparate)
+
   const dataToday = date.split(oldSeparate);
   let year;
   let mans;
@@ -25,31 +28,33 @@ function formatDate(date, fromFormat, toFormat) {
     }
   }
 
+  const resultParts = [];
+
   for (let z = 0; z < toFormat.length; z++) {
     if (toFormat[z].includes('Y') && toFormat[z].length === year.length) {
-      toFormat[z] = year;
+      resultParts[z] = year;
     } else if (toFormat[z].includes('Y') && toFormat[z].length < year.length) {
-      toFormat[z] = year.slice(-2);
+      resultParts[z] = year.slice(-2);
     } else if (
       toFormat[z].includes('Y') &&
       toFormat[z].length > year.length &&
-      year < 30
+      parseInt(year, 10) < 30
     ) {
-      toFormat[z] = '20' + year;
+      resultParts[z] = '20' + year;
     } else if (
       toFormat[z].includes('Y') &&
       toFormat[z].length > year.length &&
-      year >= 30
+      parseInt(year, 10) >= 30
     ) {
-      toFormat[z] = '19' + year;
+      resultParts[z] = '19' + year;
     } else if (toFormat[z].includes('M')) {
-      toFormat[z] = mans;
+      resultParts[z] = mans;
     } else if (toFormat[z].includes('D')) {
-      toFormat[z] = day;
+      resultParts[z] = day;
     }
   }
 
-  return toFormat.join(newSeparate);
+  return resultParts.join(newSeparate);
 }
 
 module.exports = formatDate;

292104bf9cc25caa331e2f252ef889abf79f5206
InhaKr <inkr3891@gmail.com>
2025-05-25 14:25:10 +0300

    solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,55 +8,48 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  let qwer;
-  let d;
-  const v = fromFormat[fromFormat.length - 1];
+  const oldSeparate = fromFormat.splice(3, 1);
+  const newSeparate = toFormat.splice(3, 1);
+  const dataToday = date.split(oldSeparate);
+  let year;
+  let mans;
+  let day;
 
-  const r = date.split(v);
-
-  for (let i = 0; i < r.length; i++) {
-    for (let k = 0; k < toFormat.length - 1; k++) {
-      d = toFormat[toFormat.length - 1];
-
-      if (toFormat[k] === fromFormat[i]) {
-        toFormat[k] = r[i];
-      }
-
-      if (toFormat[k] < fromFormat[i]) {
-        for (const char of toFormat[k]) {
-          for (const xar of fromFormat[i]) {
-            if (xar === char) {
-              const raw = r[i].split('').splice(2, 2).join('');
-
-              toFormat[k] = raw;
-            }
-          }
-        }
-      }
-
-      if (toFormat[k].length > fromFormat[i].length) {
-        for (const rad of toFormat[k]) {
-          for (const dar of fromFormat[i]) {
-            if (dar === rad) {
-              qwer = r[i];
-            }
-
-            if (qwer < 30) {
-              toFormat[k] = '20' + qwer;
-            }
-
-            if (qwer >= 30) {
-              toFormat[k] = '19' + qwer;
-            }
-          }
-        }
-      }
+  for (let i = 0; i < dataToday.length; i++) {
+    if (fromFormat[i].includes('Y')) {
+      year = dataToday[i];
+    } else if (fromFormat[i].includes('M')) {
+      mans = dataToday[i];
+    } else if (fromFormat[i].includes('D')) {
+      day = dataToday[i];
     }
   }
 
-  toFormat.length = 3;
+  for (let z = 0; z < toFormat.length; z++) {
+    if (toFormat[z].includes('Y') && toFormat[z].length === year.length) {
+      toFormat[z] = year;
+    } else if (toFormat[z].includes('Y') && toFormat[z].length < year.length) {
+      toFormat[z] = year.slice(-2);
+    } else if (
+      toFormat[z].includes('Y') &&
+      toFormat[z].length > year.length &&
+      year < 30
+    ) {
+      toFormat[z] = '20' + year;
+    } else if (
+      toFormat[z].includes('Y') &&
+      toFormat[z].length > year.length &&
+      year >= 30
+    ) {
+      toFormat[z] = '19' + year;
+    } else if (toFormat[z].includes('M')) {
+      toFormat[z] = mans;
+    } else if (toFormat[z].includes('D')) {
+      toFormat[z] = day;
+    }
+  }
 
-  return toFormat.join(d);
+  return toFormat.join(newSeparate);
 }
 
 module.exports = formatDate;
